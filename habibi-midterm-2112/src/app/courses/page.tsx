6d752ea7ae893b4a5b46c88b7777b0626c56a12e
Obi <obi6745@gmail.com>
2025-10-27 09:38:57 -0400

    step 8 done

--- habibi-midterm-2112/src/app/courses/page.tsx
+++ habibi-midterm-2112/src/app/courses/page.tsx
@@ -1,12 +1,26 @@
+import CourseInfo from "../component/CourseInfo";
+
 export default function Courses() {
+  const courses = [
+    "COMP 2112 React.js",
+    "COMP1072 Embedded Media", 
+    "COMP 2084 ASP.net",
+    "COMP 2081 Advanced CSS",
+    "COMP 2143 Digital Marketing"
+  ];
+
+  return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
-            <h1 className="text-4xl font-bold text-black dark:text-white mb-6">
-                Courses
-
-            </h1>
-        </main>
-
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
+        <h1 className="text-4xl font-bold text-black dark:text-white mb-6">
+          My Courses
+        </h1>
+        <ol className="list-decimal list-inside space-y-2">
+          {courses.map((course, index) => (
+            <CourseInfo key={course} courseName={course} />
+          ))}
+        </ol>
+      </main>
     </div>
-    
+  );
 }
